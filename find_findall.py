77dd3d2f5cee0983df7d97d7512df9be53e3632d
kinfolklives <gracedays@naver.com>
2020-09-22 15:42:09 +0900

    commit 5

--- find_findall.py
+++ find_findall.py
@@ -4,4 +4,16 @@ import requests
 with open('datas/sample02.html') as fp:
     soup = BeautifulSoup(fp, features='lxml')
 
-print(type(soup.find(id='link3')), soup.find(id="link3"))
+# print(type(soup.find(id='link3')), soup.find(id="link3"))
+
+# print(type(soup.find_all(name='a')), soup.find_all(name='a'))
+
+# print(type(soup.find_all(name='b')), soup.find_all(name='b'))
+
+# print(type(soup.find_all(name=["a","b"])), soup.find_all(name=["a", "b"]))
+
+# print(type(soup.find_all(id=True)), soup.find_all(id=True))
+
+find_attrs = soup.find_all(name='a', attrs={"href":True})
+print(type(find_attrs), find_attrs)
+

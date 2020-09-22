e198a37bcc40f9dfbecb119ed6cffd4322cdeae7
kinfolklives <gracedays@naver.com>
2020-09-22 16:45:30 +0900

    commit 7

--- navigatingtheTree2.py
+++ navigatingtheTree2.py
@@ -6,12 +6,12 @@ with open('datas/sample02.html') as fp:
 
 # Going sideways- .next_sibling and .next_siblings
 link = soup.a
-print(type(link.next_sibling), link.next_sibling)
+#print(type(link.next_sibling), link.next_sibling)
 
 print(type(link.next_sibling.next_sibling), link.next_sibling.next_sibling)
 
-for sibling in soup.a.next_siblings:
-    print(type(sibling), repr(sibling)) # return a printable representation 
+# for sibling in soup.a.next_siblings:
+#     print(type(sibling), repr(sibling)) # return a printable representation 
 
 
 
--- navigatingtheTree3.py
+++ navigatingtheTree3.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-res = requests.get("https://www.google.com/search?q=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&oq=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&aqs=chrome..69i57.1323j0j15&sourceid=chrome&ie=UTF-8")
+res = requests.get("https://www.google.com/search?sxsrf=ALeKk03iw1Oow586L9OL8Bn6on-zu6OwcQ%3A1600752835436&ei=w4xpX-6cGqPEmAWv5IWwCg&q=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&oq=%EC%B7%A8%EC%97%85%EC%82%AC%EC%9D%B4%ED%8A%B8&gs_lcp=CgZwc3ktYWIQAzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQRzIECAAQR1AAWABgw5rPAmgAcAF4AIABAIgBAJIBAJgBAKoBB2d3cy13aXrIAQjAAQE&sclient=psy-ab&ved=0ahUKEwiuioSBhfzrAhUjIqYKHS9yAaYQ4dUDCA0&uact=5")
 soup = BeautifulSoup(res.content, features="lxml")
 
 link = soup.a
-
-for sibling in soup.a.next_siblings:
+print(link)
+for sibling in link.next_siblings:
     print(type(sibling), sibling)
+

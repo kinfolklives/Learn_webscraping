6e825303b6812b24626320bffb3f7d26eb24883a
kinfolklives <69297065+kinfolklives@users.noreply.github.com>
2020-09-27 13:38:14 +0900

    db changed

--- miniProject/naverMovie.py
+++ miniProject/naverMovie.py
@@ -43,7 +43,7 @@ for x in range(0, 21):
 
     # DB
     for moviedata in zip(NUMBER, TITLE, REVIEW, SCORE):
-        with sqlite3.connect("sqlite/task01") as con:
+        with sqlite3.connect("C:\Develop\learn_webscraping\sqlite\db.choi") as con:
             cursor = con.cursor()
             query = """
                 insert into TASK01 (NUMBER, TITLE, REVIEW, SCORE)
